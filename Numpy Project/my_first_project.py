31b468b3eb7a5cc372b20357484024cffd19b1bd
Nilesh091 <117636145+Nilesh091@users.noreply.github.com>
2023-02-16 23:01:45 +0530

    Add files via upload

---
+++ Numpy Project/my_first_project.py
@@ -0,0 +1,7 @@
+import numpy as np
+vectorAsArray=np.array([1,2,3,4,5])
+rowVector=np.array([[1,2,3,4,5] ])
+colVector=np.array([[1],[2],[3],[4],[5]])
+print(vectorAsArray)
+print(rowVector)
+print(colVector)
---
+++ Numpy Project/my_second_project.py
@@ -0,0 +1,9 @@
+import numpy as np
+a=np.array([1,2,3,4,5])
+b=np.array([3,4,5,6,7])
+c=np.array([[1],[2],[3],[4],[5]])
+d=np.array([[4],[5],[6],[7],[8]])
+print(a+b)
+print(a-b)
+print(b-a)
+print(c+d)
