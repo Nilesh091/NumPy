84f535d67d418929babd1649681653f1bb32f6b4
Nilesh091 <117636145+Nilesh091@users.noreply.github.com>
2023-02-16 23:28:44 +0530

    Update h1.py

--- 2/h1.py
+++ 2/h1.py
@@ -1,6 +1,6 @@
 import numpy as np
 a=[1,2,3,4,5]
 b=2
-print(a*b)
+print(a*b)#prints b times the list a
 c=np.array(a)
-print(c*b)
+print(c*b)#prints the array c with all elements multiplied with the value of b
